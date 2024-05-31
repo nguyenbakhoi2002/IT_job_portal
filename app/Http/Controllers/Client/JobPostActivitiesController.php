f4203bc3a1ca8b3aa0b13801e9c50f3c189c804c
nguyenbakhoi2002 <134726066+nguyenbakhoi2002@users.noreply.github.com>
2024-05-31 19:32:10 +0700

    da co is_change 100 phan tram

--- app/Http/Controllers/Client/JobPostActivitiesController.php
+++ app/Http/Controllers/Client/JobPostActivitiesController.php
@@ -50,116 +50,186 @@ class JobPostActivitiesController extends Controller
             $client = auth('candidate')->user();
             // Nhân bản profile hiện tại của ứng viên
             $currentProfile = $client->seekerProfile()->where('is_clone', 0)->first();
-            $clonedProfile = $currentProfile->replicate();
-            $clonedProfile->is_clone = 1;
-            $clonedProfile->save();
+            $currentProfile_check_change = $currentProfile->is_change;
+            $currentProfile_hasClone = $client->seekerProfile()
+                                                ->where('is_clone', 1)
+                                                ->orderBy('created_at', 'desc')
+                                                ->first();
+            //nếu chưa có clone hoặc is_change = 1 thì tạo clone
+            if(!$currentProfile_hasClone || $currentProfile_check_change == 1){
+                $clonedProfile = $currentProfile->replicate();
+                $clonedProfile->is_clone = 1;
+                $clonedProfile->save();
 
-            //học vấn
-            if($currentProfile->educations){
-                // Nhân bản bản ghi trong bảng education tham chiếu tới profile hiện tại
-                foreach ($currentProfile->educations as $currentEducation) {
-                    $clonedEducation = $currentEducation->replicate();
-                    $clonedEducation->seeker_profile_id = $clonedProfile->id; // Liên kết với profile mới
-                    $clonedEducation->save();
-                    //so sánh bằng cấp bản clone với yêu cầu trong post 
-                    if((!in_array('edu', $satisfy))&&($clonedEducation->degree->level>=$required_degree)){
-                        $satisfy[]='edu';
+                //học vấn
+                if($currentProfile->educations){
+                    // Nhân bản bản ghi trong bảng education tham chiếu tới profile hiện tại
+                    foreach ($currentProfile->educations as $currentEducation) {
+                        $clonedEducation = $currentEducation->replicate();
+                        $clonedEducation->seeker_profile_id = $clonedProfile->id; // Liên kết với profile mới
+                        $clonedEducation->save();
+                        //so sánh bằng cấp bản clone với yêu cầu trong post 
+                        if((!in_array('edu', $satisfy))&&($clonedEducation->degree->level>=$required_degree)){
+                            $satisfy[]='edu';
+                        }
+                    }
+                    
+                }
+                //kinh nghiệm
+                if($currentProfile->experiences){
+                    foreach ($currentProfile->experiences as $currentExp) {
+                        $clonedExp = $currentExp->replicate();
+                        $clonedExp->seeker_profile_id = $clonedProfile->id; // Liên kết với profile mới
+                        $clonedExp->save();
+                    }
+                    
+                }
+                //so sánh kinh nghiệm 
+                if((!in_array('exp', $satisfy))&&($clonedProfile->total_experience>=$required_exp)){
+                    $satisfy[]='exp';
+                    
+                }
+                //kĩ năng
+                $seekerSkillClone=[];
+                if($currentProfile->skills){
+                    foreach ($currentProfile->skills as $skill) {
+                        //thêm vào bảng trung gian
+                        $clonedProfile->skills()->attach($skill->id);
+                    }
+                    $seekerSkillClone=$clonedProfile->skills->pluck('id')->toArray();
+                }
+                //so sánh kĩ năng
+                //trả về tru nếu tất cả kĩ năng trong bài tuyển nằm trong kĩ nằm của ứng viên
+                if(empty(array_diff($required_skill, $seekerSkillClone))){
+                    $satisfy[]='skill';
+                }
+
+                //ngoại ngữ
+                if($currentProfile->languages){
+                    //lấy ra tất cả bản ngôn ngữ có trong profile
+                    $seekerLanguages = SeekerLanguage::where('seeker_profile_id', $currentProfile->id)->get();
+                    foreach ($seekerLanguages as $seekerLanguage) {
+                        // Tạo bản sao của SeekerLanguage
+                        $clonedSeekerLanguage = $seekerLanguage->replicate();
+                        $clonedSeekerLanguage->seeker_profile_id = $clonedProfile->id; // Gán id của bản sao của SeekerProfile mới
+                        $clonedSeekerLanguage->save();
+                    }
+                }
+                //so sánh ngoại ngữ
+                    $check_language=true;
+                    //lấy ra tất cả yêu cầu ngôn ngữ của bài đăng 
+                    $jobPostLanguages = JobPostLanguage::where('job_post_id', $id)->get();
+                    //nếu ko có yêu cầu ngôn ngữ trả về true luôn không cần so sánh
+                    if($jobPostLanguages->count()==0) $check_language=true;
+                    else{
+                        //tất cả ngôn ngữ có trong profile clone
+                        $seekerCloneLanguages = SeekerLanguage::where('seeker_profile_id', $clonedProfile->id)->get();
+                        //nếu không có ngoại ngữ trả về false luôn, không cần so sánh
+                        if($seekerCloneLanguages->count()==0) $check_language=false;
+                        foreach ($jobPostLanguages as $jobPostLanguage) {
+                            // Tìm mức độ ngôn ngữ của ứng viên tương ứng với ngôn ngữ yêu cầu
+                            $seekerCloneLanguage = $seekerCloneLanguages->where('language_id', $jobPostLanguage->language_id)->first();
+                        
+                            // Kiểm tra xem mức độ ngôn ngữ của ứng viên có đáp ứng được yêu cầu không
+                            if ($seekerCloneLanguage && $seekerCloneLanguage->level >= $jobPostLanguage->level) {
+                                // Nếu đáp ứng được, tiếp tục kiểm tra các yêu cầu khác
+                                continue;
+                            } else {
+                                // Nếu không đáp ứng được, trả về false hoặc thực hiện hành động phù hợp
+                                $check_language=false;
+                                break;
+                            }
+                        }   
+                    }
+                    
+                    if ($check_language){
+                        $satisfy[]='language';
+                    }
+                //project
+                if($currentProfile->projects){
+                    foreach ($currentProfile->projects as $currentProject) {
+                        $clonedProject = $currentProject->replicate();
+                        $clonedProject->seeker_profile_id = $clonedProfile->id; // Liên kết với profile mới
+                        $clonedProject->save();
                     }
                 }
                 
-            }
-            //kinh nghiệm
-            if($currentProfile->experiences){
-                foreach ($currentProfile->experiences as $currentExp) {
-                    $clonedExp = $currentExp->replicate();
-                    $clonedExp->seeker_profile_id = $clonedProfile->id; // Liên kết với profile mới
-                    $clonedExp->save();
-                }
+                $jobPostActivities = new JobPostActivity();
+                $jobPostActivities->seeker_profile_id = $clonedProfile->id;
+                $jobPostActivities->job_post_id = $id;
+                $jobPostActivities->company_id = $company_id;
+                $jobPostActivities->satisfy = implode('|', $satisfy); // Chuyển mảng $satisfy thành chuỗi ngăn cách bởi dấu '|'
+                $jobPostActivities->save();
                 
             }
-            //so sánh kinh nghiệm 
-            if((!in_array('exp', $satisfy))&&($clonedProfile->total_experience>=$required_exp)){
-                $satisfy[]='exp';
-                
-            }
-            //kĩ năng
-            $seekerSkillClone=[];
-            if($currentProfile->skills){
-                foreach ($currentProfile->skills as $skill) {
-                    //thêm vào bảng trung gian
-                    $clonedProfile->skills()->attach($skill->id);
-                }
-                $seekerSkillClone=$clonedProfile->skills->pluck('id')->toArray();
-            }
-            //so sánh kĩ năng
-            //trả về tru nếu tất cả kĩ năng trong bài tuyển nằm trong kĩ nằm của ứng viên
-            if(empty(array_diff($required_skill, $seekerSkillClone))){
-                $satisfy[]='skill';
-            }
-
-            //ngoại ngữ
-            if($currentProfile->languages){
-                //lấy ra tất cả bản ngôn ngữ có trong profile
-                $seekerLanguages = SeekerLanguage::where('seeker_profile_id', $currentProfile->id)->get();
-                foreach ($seekerLanguages as $seekerLanguage) {
-                    // Tạo bản sao của SeekerLanguage
-                    $clonedSeekerLanguage = $seekerLanguage->replicate();
-                    $clonedSeekerLanguage->seeker_profile_id = $clonedProfile->id; // Gán id của bản sao của SeekerProfile mới
-                    $clonedSeekerLanguage->save();
-                }
-            }
-            //so sánh ngoại ngữ
-                $check_language=true;
-                //lấy ra tất cả yêu cầu ngôn ngữ của bài đăng 
-                $jobPostLanguages = JobPostLanguage::where('job_post_id', $id)->get();
-                //nếu ko có yêu cầu ngôn ngữ trả về true luôn không cần so sánh
-                if($jobPostLanguages->count()==0) $check_language=true;
-                else{
-                    //tất cả ngôn ngữ có trong profile clone
-                    $seekerCloneLanguages = SeekerLanguage::where('seeker_profile_id', $clonedProfile->id)->get();
-                    //nếu không có ngoại ngữ trả về false luôn, không cần so sánh
-                    if($seekerCloneLanguages->count()==0) $check_language=false;
-                    foreach ($jobPostLanguages as $jobPostLanguage) {
-                        // Tìm mức độ ngôn ngữ của ứng viên tương ứng với ngôn ngữ yêu cầu
-                        $seekerCloneLanguage = $seekerCloneLanguages->where('language_id', $jobPostLanguage->language_id)->first();
-                    
-                        // Kiểm tra xem mức độ ngôn ngữ của ứng viên có đáp ứng được yêu cầu không
-                        if ($seekerCloneLanguage && $seekerCloneLanguage->level >= $jobPostLanguage->level) {
-                            // Nếu đáp ứng được, tiếp tục kiểm tra các yêu cầu khác
-                            continue;
-                        } else {
-                            // Nếu không đáp ứng được, trả về false hoặc thực hiện hành động phù hợp
-                            $check_language=false;
+            //nếu không chỉ cần lấy clone mới nhất để nộp
+            else{
+                //học vấn
+                if($currentProfile_hasClone->educations){
+                    // Nhân bản bản ghi trong bảng education tham chiếu tới profile hiện tại
+                    foreach ($currentProfile->educations as $currentEducation) {
+                        //so sánh bằng cấp với yêu cầu trong post 
+                        if((!in_array('edu', $satisfy))&&($currentEducation->degree->level>=$required_degree)){
+                            $satisfy[]='edu';
                             break;
                         }
-                    }   
+                    }
                 }
-                
-                if ($check_language){
-                    $satisfy[]='language';
+                //kinh nghiệm
+                if((!in_array('exp', $satisfy))&&($currentProfile_hasClone->total_experience>=$required_exp)){
+                    $satisfy[]='exp';
                 }
-            //project
-            if($currentProfile->projects){
-                foreach ($currentProfile->projects as $currentProject) {
-                    $clonedProject = $currentProject->replicate();
-                    $clonedProject->seeker_profile_id = $clonedProfile->id; // Liên kết với profile mới
-                    $clonedProject->save();
+                //kĩ năng
+                $seekerSkillClone=[];
+                if($currentProfile_hasClone->skills){
+                    $seekerSkillClone=$currentProfile_hasClone->skills->pluck('id')->toArray();
                 }
+                if(empty(array_diff($required_skill, $seekerSkillClone))){
+                    $satisfy[]='skill';
+                }
+                //ngoại ngữ
+                $check_language=true;
+                    //lấy ra tất cả yêu cầu ngôn ngữ của bài đăng 
+                    $jobPostLanguages = JobPostLanguage::where('job_post_id', $id)->get();
+                    //nếu ko có yêu cầu ngôn ngữ trả về true luôn không cần so sánh
+                    if($jobPostLanguages->count()==0) $check_language=true;
+                    else{
+                        //tất cả ngôn ngữ có trong profile 
+                        $seekerCloneLanguages = SeekerLanguage::where('seeker_profile_id', $currentProfile_hasClone->id)->get();
+                        //nếu không có ngoại ngữ trả về false luôn, không cần so sánh
+                        if($seekerCloneLanguages->count()==0) $check_language=false;
+                        foreach ($jobPostLanguages as $jobPostLanguage) {
+                            // Tìm mức độ ngôn ngữ của ứng viên tương ứng với ngôn ngữ yêu cầu
+                            $seekerCloneLanguage = $seekerCloneLanguages->where('language_id', $jobPostLanguage->language_id)->first();
+                            // Kiểm tra xem mức độ ngôn ngữ của ứng viên có đáp ứng được yêu cầu không
+                            if ($seekerCloneLanguage && $seekerCloneLanguage->level >= $jobPostLanguage->level) {
+                                // Nếu đáp ứng được, tiếp tục kiểm tra các yêu cầu khác
+                                continue;
+                            } else {
+                                // Nếu không đáp ứng được, trả về false 
+                                $check_language=false;
+                                break;
+                            }
+                        }   
+                    }
+                    
+                    if ($check_language){
+                        $satisfy[]='language';
+                    }
+                //thêm vào bảng trung gian
+                $jobPostActivities = new JobPostActivity();
+                $jobPostActivities->seeker_profile_id = $currentProfile_hasClone->id;
+                $jobPostActivities->job_post_id = $id;
+                $jobPostActivities->company_id = $company_id;
+                $jobPostActivities->satisfy = implode('|', $satisfy); // Chuyển mảng $satisfy thành chuỗi ngăn cách bởi dấu '|'
+                $jobPostActivities->save();
             }
-            
-            $jobPostActivities = new JobPostActivity();
-            $jobPostActivities->seeker_profile_id = $clonedProfile->id;
-            $jobPostActivities->job_post_id = $id;
-            $jobPostActivities->company_id = $company_id;
-            $jobPostActivities->satisfy = implode('|', $satisfy); // Chuyển mảng $satisfy thành chuỗi ngăn cách bởi dấu '|'
-            $jobPostActivities->save();
-
-
-            
-
-            // Hoàn thành giao dịch
+            //sau đó update is_change về 0; 
+            $currentProfile->update([
+                'is_change'=>0,
+            ]);
             DB::commit();
-            return redirect()->back()->with('success', 'thêm mới thành công');
+            return redirect()->back()->with('success', 'ứng tuyển thành công');
             // return response()->json([
             //     'success' => 'Ứng tuyển thành công!',
             // ]);
